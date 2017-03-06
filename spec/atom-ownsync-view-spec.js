4ffd108e06f7e5b6d7bb964c8d4cb09d57a97bbe
Andrew Andrianov <andrew@ncrmnt.org>
2017-03-06 21:58:44 +0300

    Initial commit of the first prototype
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ spec/atom-ownsync-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomOwnsyncView from '../lib/atom-ownsync-view';
+
+describe('AtomOwnsyncView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
